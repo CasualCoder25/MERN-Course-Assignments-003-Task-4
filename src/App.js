ea54fe2f15ca6d6dd7c6f228e45530abe08853a6
CasualCoder25 <h.rajanna2505@gmail.com>
2023-10-13 18:11:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import DataTable from "./components/DataTable"
 
 function App() {
+  const AppStyle = {
+    width: "100%",
+    height: "100%",
+    minHeight: "100vh",
+    fontSize: "0.8rem",
+  }
+  const DataTableStyle = {
+    width: "100%",
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="bg-d-black d-flex flex-column align-items-center text-light"
+      style={AppStyle}
+    >
+      <h1 className="p-1">Dummy data</h1>
+      <div style={DataTableStyle}>
+        <DataTable />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
